f7f028b8e99e2f8d4285c8a8e1fca98ea60d889b
josekameydev01 <gk01cracklitos@gmail.com>
2025-09-16 19:18:51 -0600

    first commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by carlosgalvankamey on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
