bb61f138036c0ce426f1e0b89da630bd4c9b633f
josekameydev01 <gk01cracklitos@gmail.com>
2025-09-19 12:43:20 -0600

    customizing spring

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,29 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 2.0
+    @State private var animationAmount = 1.0
     var body: some View {
         Button("Tap Me") {
-            
+            animationAmount += 0.1
         }
         .padding(50)
         .background(.red)
         .foregroundStyle(.white)
         .clipShape(.circle)
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeInOut(duration: 1)
-                    .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 2
-        }
+        .scaleEffect(animationAmount)
+        .animation(.spring(duration: 1, bounce: 0.9), value: animationAmount)
     }
 }
 
