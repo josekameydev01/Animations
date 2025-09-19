69a8cb593a0480482f5d5af1a45614409cd02788
josekameydev01 <gk01cracklitos@gmail.com>
2025-09-19 13:09:17 -0600

    how to make an animation that start immediately

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -11,18 +11,26 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
         Button("Tap Me") {
-            animationAmount += 0.1
+//            animationAmount += 0.1
         }
         .padding(50)
         .background(.red)
         .foregroundStyle(.white)
         .clipShape(.circle)
-        .scaleEffect(animationAmount)
-        .animation(
-            .easeInOut(duration: 2)
-            .repeatForever(autoreverses: true),
-            value: animationAmount
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeInOut(duration: 2)
+                    .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
         )
+        .onAppear {
+            animationAmount = 2.0
+        }
     }
 }
 
