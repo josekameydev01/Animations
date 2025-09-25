a1e65b67f44cb386b94afb88cea8643de262e957
josekameydev01 <gk01cracklitos@gmail.com>
2025-09-24 22:17:10 -0600

    how to get a default view transition

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,28 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    private var letters = Array("Hello Swift")
-    
-    @State private var dragAmounts: [CGSize] = Array(repeating: .zero, count: 11)
-    @State private var enableStates: [Bool] = Array(repeating: false, count: 11)
-    
+    @State private var isShowingRed = false
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count, id: \.self) { num in
-                Text("\(letters[num])")
-                    .padding(5)
-                    .font(.title)
-                    .foregroundStyle(enableStates[num] ? .green : .black)
-                    .offset(dragAmounts[num])
-                    .animation(.linear, value: dragAmounts[num])
-                    .gesture(
-                        DragGesture()
-                            .onChanged { dragAmounts[num] = $0.translation }
-                            .onEnded { _ in
-                                dragAmounts[num] = .zero
-                                enableStates[num].toggle()
-                            }
-                    )
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
             }
         }
     }
