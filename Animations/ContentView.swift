bc423b0d8fc51741fc4ec37402247056414cb4cf
josekameydev01 <gk01cracklitos@gmail.com>
2025-09-24 22:20:45 -0600

    how to use the transition modifier

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -21,6 +21,7 @@ struct ContentView: View {
                 Rectangle()
                     .fill(.red)
                     .frame(width: 200, height: 200)
+                    .transition(.scale)
             }
         }
     }
